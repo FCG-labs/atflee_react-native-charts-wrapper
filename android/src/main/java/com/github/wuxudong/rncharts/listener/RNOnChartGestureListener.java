bd164842dfe95f01e1e18f21093e706f97b250a7
FCG <143424305+FCG-labs@users.noreply.github.com>
2025-06-08 03:52:51 +0900

    -

--- android/src/main/java/com/github/wuxudong/rncharts/listener/RNOnChartGestureListener.java
+++ android/src/main/java/com/github/wuxudong/rncharts/listener/RNOnChartGestureListener.java
@@ -133,7 +133,7 @@ public class RNOnChartGestureListener implements OnChartGestureListener {
 
             float minX = chart.getData() != null ? chart.getData().getXMin() : Float.MIN_VALUE;
             float maxX = chart.getData() != null ? chart.getData().getXMax() : Float.MAX_VALUE;
-            float dragOffset = 30f; //viewPortHandler.getDragOffsetX();
+            float dragOffset = 30f;
 
             double allowedMin = minX - dragOffset;
             double allowedMax = maxX + dragOffset;
