058cb02b0f1974069bb9bad94e996b0be7d40b46
FCG <143424305+FCG-labs@users.noreply.github.com>
2025-06-17 19:58:28 +0900

    Ensure minimum visibleRange

--- ios/ReactNativeCharts/RNBarLineChartViewBase.swift
+++ ios/ReactNativeCharts/RNBarLineChartViewBase.swift
@@ -117,8 +117,10 @@ class RNBarLineChartViewBase: RNYAxisChartViewBase {
         let json = BridgeUtils.toJson(config)
 
         let x = json["x"]
+        var minRange: Double? = nil
         if x["min"].double != nil {
-            barLineChart.setVisibleXRangeMinimum(x["min"].doubleValue)
+            minRange = x["min"].doubleValue
+            barLineChart.setVisibleXRangeMinimum(minRange!)
         }
         if x["max"].double != nil {
             barLineChart.setVisibleXRangeMaximum(x["max"].doubleValue)
@@ -137,6 +139,14 @@ class RNBarLineChartViewBase: RNYAxisChartViewBase {
         }
         if y["right"]["max"].double != nil {
             barLineChart.setVisibleYRangeMaximum(y["right"]["max"].doubleValue, axis: YAxis.AxisDependency.right)
+        }
+
+        if let range = minRange {
+            let axis = barLineChart.xAxis
+            if axis.axisMaximum - axis.axisMinimum < range {
+                axis.axisMaximum = axis.axisMinimum + range
+                barLineChart.notifyDataSetChanged()
+            }
         }
 
         sendEvent("chartLoadComplete")
