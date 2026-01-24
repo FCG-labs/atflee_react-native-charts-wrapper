7628712aa6b73add3e208cd9ea6f5ec412f55805
FCG <143424305+FCG-labs@users.noreply.github.com>
2026-01-24 19:43:45 +0900

    Fix panGestureRecognizer access in nested scrolling

--- ios/ReactNativeCharts/bar/AtfleeBarChart.swift
+++ ios/ReactNativeCharts/bar/AtfleeBarChart.swift
@@ -13,7 +13,8 @@ class AtfleeBarChart: BarChartView {
     override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
         // Use NestedScrollingHelper to determine if we should consume the gesture
         // or let it pass through to the parent (e.g. ScrollView)
-        if !NestedScrollingHelper.shouldRecognizeGesture(gestureRecognizer, in: self, panGesture: self.panGestureRecognizer) {
+        let panGesture = gestureRecognizer as? UIPanGestureRecognizer
+        if !NestedScrollingHelper.shouldRecognizeGesture(gestureRecognizer, in: self, panGesture: panGesture) {
             return false
         }
         return super.gestureRecognizerShouldBegin(gestureRecognizer)
