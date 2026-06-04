5105b855a27d27b4de302efae9608688b5926d87
FCG <143424305+FCG-labs@users.noreply.github.com>
2026-06-04 15:43:58 +0900

    fix(ios): add umbrella header for Swift compatibility import

---
+++ ios/ReactNativeCharts/ReactNativeCharts.h
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double ReactNativeChartsVersionNumber;
+FOUNDATION_EXPORT const unsigned char ReactNativeChartsVersionString[];
